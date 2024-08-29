8b41581f44a076f978c497d1a52c0ce5a3e30d7d
Tanya Ten <tanya@14m3pro.local>
2024-08-30 00:46:37 +0300

    first iter

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,88 @@
 package main
 
-func main() {}
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"sync"
+
+	"github.com/mr-tron/base58"
+)
+
+var UrlStorage = struct {
+	sync.RWMutex
+	m map[string]string
+}{m: make(map[string]string)}
+
+func generateShortString(s string) string {
+	// 1. Создаем хэш SHA-256
+	h := sha256.New()
+	h.Write([]byte(s))
+	hashBytes := h.Sum(nil)
+
+	// 2. Преобразовываем хэш в строку Hex
+	hexString := hex.EncodeToString(hashBytes)
+
+	// 3. Используем Base58 для кодирования Hex-строки
+	encodedString := base58.Encode([]byte(hexString))
+
+	// 4. Сокращаем до длины 8 символов (или другой необходимой длины)
+	if len(encodedString) > 8 {
+		return encodedString[:8]
+	}
+	return encodedString
+}
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Не удалось прочитать тело запроса", http.StatusInternalServerError)
+			return
+		}
+		defer r.Body.Close()
+		bodyLink := string(body)
+		newLink := generateShortString(bodyLink)
+
+		UrlStorage.Lock()
+		UrlStorage.m[newLink] = bodyLink
+		UrlStorage.Unlock()
+
+		fmt.Println(UrlStorage.m)
+		w.Header().Set("content-type", "text/plain")
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte("Short URL: " + newLink))
+		return
+	} else if r.Method == http.MethodGet {
+		url := strings.TrimPrefix(r.URL.Path, "/")
+
+		UrlStorage.RLock()
+		originalUrl, ok := UrlStorage.m[url]
+		UrlStorage.RUnlock()
+
+		fmt.Println("originalUrl", originalUrl)
+		if ok {
+			fmt.Println("GET: Found URL:", originalUrl)
+			w.Header().Set("Location", originalUrl)
+			w.WriteHeader(http.StatusTemporaryRedirect)
+		} else {
+			fmt.Println("GET: Key not found:", url)
+			http.Error(w, "URL не найден", http.StatusNotFound)
+		}
+	} else {
+		http.Error(w, "Метод не поддерживается", http.StatusMethodNotAllowed)
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, mainPage)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/thalq/url-service
+
+go 1.22.5
+
+require github.com/mr-tron/base58 v1.2.0 // indirect
