72ab6b77f48feee34c43c56322697b627c1163c9
Tanya Ten <izvestiya9@mail.ru>
2024-09-06 14:55:49 +0300

    fix

--- cmd/shortener/url_validator.go
+++ cmd/shortener/url_validator.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"net/url"
 )
 
 func ifValidURL(testURL string) bool {
 	parsedURL, err := url.ParseRequestURI(testURL)
-	fmt.Println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!", parsedURL)
 	if err != nil {
 		return false
 	}
